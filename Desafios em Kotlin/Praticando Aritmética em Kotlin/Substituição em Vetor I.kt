1187732471a6f7f3efeb5c8166a0a66250ab92b1
raphael-pestana <65823943+raphael-pestana@users.noreply.github.com>
2021-07-28 15:30:30 -0300

    Update Substituição em Vetor I.kt

--- Desafios em Kotlin/Praticando Aritmética em Kotlin/Substituição em Vetor I.kt
+++ Desafios em Kotlin/Praticando Aritmética em Kotlin/Substituição em Vetor I.kt
@@ -1,24 +1,23 @@
 /* Desafio
-Você recebeu o desafio de ler um valor e criar um programa que coloque o valor lido na primeira posição de um vetor
-N[10]. Em cada posição subsequente, coloque o dobro do valor da posição anterior.
-Por exemplo, se o valor lido for 1, os valores do vetor devem ser 1,2,4,8 e assim sucessivamente.
-Mostre o vetor em seguida.
+Faça um programa que leia um vetor X[10]. Substitua a seguir, todos os valores nulos e negativos do vetor X por 1.
+Em seguida mostre o vetor X.
 
 Entrada
-A entrada contém um valor inteiro (V<=50).
+A entrada contém 10 valores inteiros, podendo ser positivos ou negativos.
 
 Saída
-Para cada posição do vetor, escreva "N[i] = X", onde i é a posição do vetor e X é o valor armazenado na posição i.
-O primeiro número do vetor N (N[0]) irá receber o valor de V.
+Para cada posição do vetor, escreva "X[i] = x", onde i é a posição do vetor e x é o valor armazenado naquela posição.
 */
 
 // CÓDIGO
 
 fun main(args: Array<String>) {
+
     val list = IntArray(10) { readLine()!!.toInt() }
 
     for (x in list.indices) {
         if (list[x] <= 0) list[x] = 1
     }
-
     for (i in list.indices) println("X[$i] = ${list[i]}")
+
+}
