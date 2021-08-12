0412447170465c4a65d1cb054b0b9c064906bc49
raphael-pestana <65823943+raphael-pestana@users.noreply.github.com>
2021-08-12 15:21:15 -0300

    Room Database configured

--- Projetos/My-Business-Card/app/src/main/java/com/pestana/mybusinesscard/ui/AddBusinessCardActivity.kt
+++ Projetos/My-Business-Card/app/src/main/java/com/pestana/mybusinesscard/ui/AddBusinessCardActivity.kt
@@ -1,12 +1,22 @@
 package com.pestana.mybusinesscard.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.pestana.mybusinesscard.App
+import com.pestana.mybusinesscard.R
+import com.pestana.mybusinesscard.data.BusinessCard
 import com.pestana.mybusinesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -19,7 +29,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
         }
 
         binding.btnConfirm.setOnClickListener {
-
+            val businessCard = BusinessCard (
+                name = binding.tilName.editText?.text.toString(),
+                company = binding.tilCompany.editText?.text.toString(),
+                number = binding.tilPhone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                cardBackgroung = binding.tilColor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT ).show()
+            finish()
         }
     }
 }
