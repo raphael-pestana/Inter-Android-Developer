27b426e4bdd28379580815349029b82c96d9ff79
raphael-pestana <65823943+raphael-pestana@users.noreply.github.com>
2021-08-19 16:32:44 -0300

    Configured ViewMoldel

--- Projetos/My-App-Repositories/app/src/main/java/br/com/dio/app/repositories/App.kt
+++ Projetos/My-App-Repositories/app/src/main/java/br/com/dio/app/repositories/App.kt
@@ -3,6 +3,7 @@ package br.com.dio.app.repositories
 import android.app.Application
 import br.com.dio.app.repositories.data.di.DataModule
 import br.com.dio.app.repositories.domain.di.DomainModule
+import br.com.dio.app.repositories.presentation.di.PresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -16,5 +17,6 @@ class App : Application() {
         }
         DataModule.load()
         DomainModule.load()
+        PresentationModule.load()
     }
 }
